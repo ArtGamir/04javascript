d244511ac9c8636fed08cff04bd8ee476c33a91e
Armando García <art.gamir@gmail.com>
2023-10-12 22:04:24 -0600

    First Commit

---
+++ s1variablesFunciones/main.js
@@ -0,0 +1,16 @@
+console.log("hola koders")
+
+let a=5;
+let b=3;
+
+let result1 = a+b
+let result2 = a-b
+let result3 = a/b
+let result4 = a*b
+let result5 = a%b
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
+console.log(result4)
+console.log(result5)
