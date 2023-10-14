a8337677eeb06bf24275255fc1e5d9415049d701
Armando García <art.gamir@gmail.com>
2023-10-13 22:05:59 -0600

    Funciones aritméticas

--- Funciones/Funciones.js
+++ Funciones/Funciones.js
@@ -1,3 +1,5 @@
+/*Función Suma*/
+
 function sumaDosNumeros (numero1, numero2) {
  let result = numero1 + numero2;
     return result;
@@ -6,3 +8,57 @@ function sumaDosNumeros (numero1, numero2) {
   let suma = sumaDosNumeros (6,9)
 
 console.log(suma)
+
+/*Función Multiplicación*/
+function multiplicaDosNumeros (numero1, numero2) {
+    let result = numero1 * numero2;
+       return result;
+     }
+   
+     let multiplicacion = multiplicaDosNumeros (3,2)
+   
+   console.log(multiplicacion)
+
+
+/*Función Resta*/
+   function restaDosNumeros (numero1, numero2) {
+    let result = numero1 - numero2;
+       return result;
+     }
+   
+     let resta = restaDosNumeros (8,4)
+   
+   console.log(resta)
+
+
+/*Función División*/
+   function divideDosNumeros (numero1, numero2) {
+    let result = numero1 / numero2;
+       return result;
+     }
+   
+     let divide = divideDosNumeros (12,4)
+
+     console.log(divide)
+
+     
+/*Función Módulo*/
+     function modDosNumeros (numero1, numero2) {
+      let result = numero1 % numero2;
+         return result;
+       }
+     
+       let mod = modDosNumeros (13,4)
+     
+     console.log(mod)
+
+
+/*Función Mayor de dos números*/
+
+function mayorDeDosNumeros (suma,resta) {
+
+
+}
+
+
+!=
